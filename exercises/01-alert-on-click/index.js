821d14f748c3c2344aff8d5d55f5a22707cf9caa
Gustavo Ramírez <rngustavo@gmail.com>
2021-02-26 03:15:11 +0000

    ejerciocios 1 al 2

--- exercises/01-alert-on-click/index.js
+++ exercises/01-alert-on-click/index.js
@@ -1,3 +1,8 @@
 window.myClickFunction = function myClickFunction() {
 	alert("Your first function!");
 };
+
+const button = document.getElementById("button2");
+button.addEventListener("click", window.myClickFunction);
+
+
--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,1 +1,15 @@
 //Declare your function here
+/*const input = document.getElementById("hello");
+input.addEventListener("click", () => {
+	alert("presionado");
+});
+*/
+
+document.querySelector("#hello").onclick = function() {
+	alert("hello");
+};
+/*
+function probando() {
+	alert("hello");
+}
+*/
